3ffe03432a457b5799ecef7a337fdd3583b53863
EgilFurnes <egilsnapchat@gmail.com>
2024-11-09 13:37:17 +0100

    ferdiggjorde uke1

--- uke1.py
+++ uke1.py
@@ -32,3 +32,34 @@ def egg(Tw, Ty, M=67, rho=1.038, c=3.7, K=0.0054, T0=4):
 
 egg(Tw=100, Ty=70)
 
+# 1.2 versions fra oppgaveheftet
+
+import sys
+print("Python version:", sys.version)
+
+# 1.1 massdensity fra fysikkheftet
+
+def massdensity(weight, volume):
+    density = weight/volume
+    polystyren = 20
+    kork = 220
+    rhenium = 21020
+    platinium = 21450
+    list_values = [polystyren, kork, rhenium, platinium]
+    closest = min(list_values, key = lambda x: abs(x-density)) 
+    print(f"The closest value to {density:.0f} is {closest}")
+
+massdensity(0.858, 40/1000000)
+
+# 1.2 solarmass fra fysikkheftet
+
+def solarmass():
+    G = 6.674*10**(-11)
+    yr = 9.5*10**12
+    AU = 1.58*10**(-5)*yr
+    M = (4*np.pi**2*AU**3)/(G*yr**2)
+    print(f"the mass of the sun is {M}")
+
+solarmass()
+
+# 1.1 glucose
