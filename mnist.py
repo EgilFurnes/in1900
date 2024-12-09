a6af84aa25432ff9ecaacda6008b84c4ec7b112e
EgilFurnes <egilsnapchat@gmail.com>
2024-12-09 17:22:28 +0100

    doing stuff

--- mnist.py
+++ mnist.py
@@ -4,7 +4,21 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 data = pd.read_csv("data/mnist_train.csv")
-print(data.head(2))
 
+# print(data.head(5))
+
+data = np.array(data)
+m, n = data.shape
+np.random.shuffle(data)
+
+data_dev = data[0:1000].T
+Y_dev = data_dev[0]
+X_dev = data_dev[1:n]
+
+data_train = data[1000:m].T
+Y_train = data_train[0]
+X_train = data_train[1:n]
+
+# print(X_train[:,0].shape)
 
 
