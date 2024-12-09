28555a52f430f1800e68c70bdabcb5d7d20ce482
EgilFurnes <egilsnapchat@gmail.com>
2024-12-09 17:53:16 +0100

    msg

--- mnist.py
+++ mnist.py
@@ -19,19 +19,24 @@ data_train = data[1000:m].T
 Y_train = data_train[0]
 X_train = data_train[1:n]
 
-# print(X_train[:,0].shape)
+X_train = X_train / 255.0
+X_dev = X_dev / 255.0
+
+
+print(X_train[:,0].shape)
 
 def init_params():
-    W1 = np.random.randn(10, 784) - 0.5
-    b1 = np.random.randn(10, 1) - 0.5
-    W2 = np.random.randn(10, 10) - 0.5
-    b2 = np.random.randn(10, 1) - 0.5
+    W1 = np.random.rand(10, 784) 
+    b1 = np.random.rand(10, 1) 
+    W2 = np.random.rand(10, 10) 
+    b2 = np.random.rand(10, 1) 
     return W1, b1, W2, b2
 
 def ReLU(Z):
     return np.maximum(0, Z)
 
 def softmax(Z):
+    # exp_Z = np.exp(Z - np.max(Z, axis=0, keepdims=True))  # To avoid overflow
     return np.exp(Z) / np.sum(np.exp(Z))
 
 def forward_prop(W1, b1, W2, b2, X):
@@ -39,14 +44,54 @@ def forward_prop(W1, b1, W2, b2, X):
     A1 = ReLU(Z1)
     Z2 = W2.dot(A1) + b2
     A2 = softmax(Z2)
+    return Z1, A1, Z2, A2
 
 def one_hot(Y):
     one_hot_Y = np.zeros((Y.size, Y.max() + 1))
-    one_hot_Y[np.arrange(Y.size), Y] = 1
+    one_hot_Y[np.arange(Y.size), Y] = 1  # Correct function name here
     one_hot_Y = one_hot_Y.T
     return one_hot_Y
 
-def back_prop(Z1, A1, Z2, A2, W2, Y):
+def deriv_ReLU(Z):
+    return Z > 0
+
+def back_prop(Z1, A1, Z2, A2, W2, X, Y):
+    one_hot_Y = one_hot(Y)
+    dZ2 = A2 - one_hot_Y
+    dW2 = 1 / m * dZ2.dot(A1.T)
+    db2 = 1 / m * np.sum(dZ2, axis=1, keepdims=True)
+    dZ1 = W2.dot(dZ2) * deriv_ReLU(Z1)
+    dW1 = 1 / m * dZ1.dot(X.T)
+    db1 = 1 / m * np.sum(dZ1, axis=1, keepdims=True)  # Corrected
+    return dW1, db1, dW2, db2
+
+def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
+    W1 = W1 - alpha * dW1
+    b1 = b1 - alpha * db1
+    W2 = W2 - alpha * dW2
+    b2 = b2 - alpha * db2
+    return W1, b1, W2, b2
+
+def get_predictions(A2):
+    return np.argmax(A2, 0)
+
+def get_accuracy(predictions, Y):
+    print(predictions, Y)
+    return np.sum(predictions == Y) / Y.size  # Return accuracy
+
+def gradient_descent(X, Y, iterations, alpha):
+    W1, b1, W2, b2 = init_params()
+    for i in range(iterations):
+        Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
+        dW1, db1, dW2, db2 = back_prop(Z1, A1, Z2, A2, W2, X, Y)
+        W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+        if (i % 10 == 0):
+            print("Iteration: ", i)
+            print("Accuracy: ", get_accuracy(get_predictions(A2), Y))
+    return W1, b1, W2, b2
+
+W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 500, 0.01)  # try smaller alpha
+
 
 
 
