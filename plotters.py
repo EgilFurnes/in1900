6b8c2b559f7d9deb02c2f5c70ee9694bee343931
EgilFurnes <egilsnapchat@gmail.com>
2024-12-10 20:52:07 +0100

    yo

--- plotters.py
+++ plotters.py
@@ -113,3 +113,118 @@ y2 = f2(x)
 # plt.ylabel('y')
 # plt.show()
 
+# t = np.linspace(0, 8, 201)
+# plt.plot(x, np.exp(-x)*np.sin(2*np.pi*x), x, np.exp(-2*x)*np.sin(4*np.pi*x))
+# plt.show()
+
+from numpy import *
+import matplotlib.pyplot as plt
+
+# formula = input('write a mathematical expression using x: ')
+# xmin = float(input('lower bound for x: '))
+# xmax = float(input('upper bound for x: '))
+
+# x = linspace(xmin, xmax, 101)
+# y = eval(formula)
+# print(y)
+
+# plt.plot(x, y)
+# plt.show()
+
+print(f'\n')
+
+def H(x):
+    if x < 0:
+        return 0
+    elif x >= 0: 
+        return 1
+
+print(H(0))
+print(H(1))
+print(H(2))
+
+x = linspace(-10, 10, 5)
+# y = H(x)
+# plt.plot(x, y)
+# plt.show()
+
+n = 5
+x = np.linspace(-5, 5, n+1)
+y = np.zeros(n+1)
+
+for i in range(len(x)):
+    y[i] = H(x[i])
+
+# plt.plot(x,y)
+# plt.show()
+
+def Hl(x):
+    r = np.zeros(len(x))
+    for i in range(len(x)):
+        r[i] = H(x[i])
+    return r
+
+n = 5
+x = np.linspace(-5, 5, n+1)
+y = Hl(x)
+
+# plt.plot(x,y)
+# plt.show()
+
+Hv = np.vectorize(H)
+def Hv(x): return np.where(x<0, 0.0, 1.0)
+
+# def fvec(x): 
+#     x1 = <exp1>
+#     x2 = <exp2>
+#     r = np.where(condition, x1, x2)
+#     return r
+
+def f(x,m,s):
+    return (1.0/(np.sqrt(2*np.pi*pi)*s))*np.exp(-0.5*((x-m)/s)**2)
+
+m = 0; s_start = 2; s_stop = 0.2
+s_values = np.linspace(s_start, s_stop, 30)
+
+x = np.linspace(m-3*s_start, m+3*s_start, 1000)
+max_f = f(m, m, s_stop)
+
+y = f(x,m,s_stop)
+lines = plt.plot(x,y)
+
+plt.axis([x[0], x[-1], -0.1, max_f])
+plt.xlabel('x')
+plt.ylabel('y')
+
+# for s in s_values:
+#     y = f(x, m, s)
+#     lines[0].set_ydata(y)
+#     plt.draw()
+#     plt.pause(0.1)
+
+
+from matplotlib.animation import FuncAnimation
+def f(x,m,s):
+    return (1.0/(np.sqrt(2*np.pi*pi)*s))*np.exp(-0.5*((x-m)/s)**2)
+
+m = 0; s_start = 2; s_stop = 0.2
+s_values = np.linspace(s_start, s_stop, 30)
+x = np.linspace(-3*s_start, 3*s_start, 1000)
+max_f = f(m, m, s_stop)
+
+plt.axis([x[0], x[-1], 0, max_f])
+plt.xlabel('x'); plt.ylabel('y')
+
+y = f(x, m, s_start)
+lines = plt.plot(x, y)
+
+def next_frame(s):
+    y = f(x, m, s)
+    lines[0].set_ydata(y)
+    return lines
+
+# ani = FuncAnimation(plt.gcf(), next_frame, frames=s_values, interval=1000)
+# ani.save('movie.gif', fps=60)
+# # plt.show()
+
+
